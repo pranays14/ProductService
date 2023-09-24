8ca80321c864bf7ce006a1248d044a7b5c4623c8
pranays14 <pranay.sahu005@gmail.com>
2023-09-24 13:52:17 +0530

    adding UUID

--- src/main/java/dev/pranay/productservice/models/BaseModel.java
+++ src/main/java/dev/pranay/productservice/models/BaseModel.java
@@ -1,10 +1,15 @@
 package dev.pranay.productservice.models;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue(generator = "uuidgenerator")
+    @GenericGenerator(name = "uuidgenerator", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
+    private UUID uuid;
 }
